776e3e9b78351dbf6922644167f97ccdd74a8b8f
Jim Mussared <jim.mussared@gmail.com>
2016-06-03 15:27:14 +1000

    Add a __WFI() in the music wait loop.

--- source/microbit/microbitmusic.cpp
+++ source/microbit/microbitmusic.cpp
@@ -124,6 +124,7 @@ void microbit_music_tick(void) {
 STATIC void wait_async_music_idle(void) {
     // wait for the async music state to become idle
     while (async_music_state != ASYNC_MUSIC_STATE_IDLE) {
+        __WFI();
         // allow CTRL-C to stop the music
         if (MP_STATE_VM(mp_pending_exception) != MP_OBJ_NULL) {
             async_music_state = ASYNC_MUSIC_STATE_IDLE;
