544737969bfbe64fc38c3ef3d41706eff6cf1c28
Jim Mussared <jim.mussared@gmail.com>
2016-11-21 19:14:21 +1100

    Update modradio to use the simulator's radio support.

--- source/microbit/modradio.cpp
+++ source/microbit/modradio.cpp
@@ -24,6 +24,8 @@
  * THE SOFTWARE.
  */
 
+#include "MicroBit.h"
+
 extern "C" {
 
 #include <stdio.h>
@@ -33,7 +35,6 @@ extern "C" {
 #include "py/runtime.h"
 #include "microbitobj.h"
 
-/*
 #define RADIO_DEFAULT_MAX_PAYLOAD   (32)
 #define RADIO_DEFAULT_QUEUE_LEN     (3)
 #define RADIO_DEFAULT_CHANNEL       (7)
@@ -41,7 +42,6 @@ extern "C" {
 #define RADIO_DEFAULT_BASE0         (0x75626974) // "uBit"
 #define RADIO_DEFAULT_PREFIX0       (0)
 #define RADIO_DEFAULT_DATA_RATE     (RADIO_MODE_MODE_Nrf_1Mbit)
-*/
 
 typedef struct _radio_state_t {
     uint8_t max_payload;    // 1-251 inclusive
@@ -99,22 +99,20 @@ static void ensure_enabled(void) {
 }
 
 static void radio_disable(void) {
-  /*
-    NVIC_DisableIRQ(RADIO_IRQn);
-    NRF_RADIO->EVENTS_DISABLED = 0;
-    NRF_RADIO->TASKS_DISABLE = 1;
-    while (NRF_RADIO->EVENTS_DISABLED == 0);
+    // NVIC_DisableIRQ(RADIO_IRQn);
+    // NRF_RADIO->EVENTS_DISABLED = 0;
+    // NRF_RADIO->TASKS_DISABLE = 1;
+    // while (NRF_RADIO->EVENTS_DISABLED == 0);
     // free any old buffers
     if (MP_STATE_PORT(radio_buf) != NULL) {
         m_del(uint8_t, MP_STATE_PORT(radio_buf), buf_end - MP_STATE_PORT(radio_buf));
         MP_STATE_PORT(radio_buf) = NULL;
     }
-  */
 }
 
 static void radio_enable(void) {
     radio_disable();
-    /*
+
     // allocate tx and rx buffers
     size_t max_payload = radio_state.max_payload + 1; // an extra byte to store the length
     size_t queue_len = radio_state.queue_len + 1; // one extra for tx buffer
@@ -122,68 +120,68 @@ static void radio_enable(void) {
     buf_end = MP_STATE_PORT(radio_buf) + max_payload * queue_len;
     rx_buf = MP_STATE_PORT(radio_buf) + max_payload; // start is tx buffer
 
+    simulator_radio_config(radio_state.channel, radio_state.base0, radio_state.prefix0, radio_state.data_rate);
+
     // Enable the High Frequency clock on the processor. This is a pre-requisite for
     // the RADIO module. Without this clock, no communication is possible.
-    NRF_CLOCK->EVENTS_HFCLKSTARTED = 0;
-    NRF_CLOCK->TASKS_HFCLKSTART = 1;
-    while (NRF_CLOCK->EVENTS_HFCLKSTARTED == 0);
+    // NRF_CLOCK->EVENTS_HFCLKSTARTED = 0;
+    // NRF_CLOCK->TASKS_HFCLKSTART = 1;
+    // while (NRF_CLOCK->EVENTS_HFCLKSTARTED == 0);
 
-    // power should be one of: -30, -20, -16, -12, -8, -4, 0, 4
-    NRF_RADIO->TXPOWER = radio_state.power_dbm;
+    // // power should be one of: -30, -20, -16, -12, -8, -4, 0, 4
+    // NRF_RADIO->TXPOWER = radio_state.power_dbm;
 
-    // should be between 0 and 100 inclusive (actual physical freq is 2400MHz + this register)
-    NRF_RADIO->FREQUENCY = radio_state.channel;
+    // // should be between 0 and 100 inclusive (actual physical freq is 2400MHz + this register)
+    // NRF_RADIO->FREQUENCY = radio_state.channel;
 
-    // configure data rate
-    NRF_RADIO->MODE = radio_state.data_rate;
+    // // configure data rate
+    // NRF_RADIO->MODE = radio_state.data_rate;
 
-    // The radio supports filtering packets at the hardware level based on an address.
-    // We use a 5-byte address comprised of 4 bytes (set by BALEN=4 below) from the BASEx
-    // register, plus 1 byte from PREFIXm.APn.
-    // The (x,m,n) values are selected by the logical address.  We use logical address 0
-    // which means using BASE0 with PREFIX0.AP0.
-    NRF_RADIO->BASE0 = radio_state.base0;
-    NRF_RADIO->PREFIX0 = radio_state.prefix0;
-    NRF_RADIO->TXADDRESS = 0; // transmit on logical address 0
-    NRF_RADIO->RXADDRESSES = 1; // a bit mask, listen only to logical address 0
+    // // The radio supports filtering packets at the hardware level based on an address.
+    // // We use a 5-byte address comprised of 4 bytes (set by BALEN=4 below) from the BASEx
+    // // register, plus 1 byte from PREFIXm.APn.
+    // // The (x,m,n) values are selected by the logical address.  We use logical address 0
+    // // which means using BASE0 with PREFIX0.AP0.
+    // NRF_RADIO->BASE0 = radio_state.base0;
+    // NRF_RADIO->PREFIX0 = radio_state.prefix0;
+    // NRF_RADIO->TXADDRESS = 0; // transmit on logical address 0
+    // NRF_RADIO->RXADDRESSES = 1; // a bit mask, listen only to logical address 0
 
-    // LFLEN=8 bits, S0LEN=0, S1LEN=0
-    NRF_RADIO->PCNF0 = 0x00000008;
-    // STATLEN=0, BALEN=4, ENDIAN=0 (little), WHITEEN=1
-    NRF_RADIO->PCNF1 = 0x02040000 | radio_state.max_payload;
+    // // LFLEN=8 bits, S0LEN=0, S1LEN=0
+    // NRF_RADIO->PCNF0 = 0x00000008;
+    // // STATLEN=0, BALEN=4, ENDIAN=0 (little), WHITEEN=1
+    // NRF_RADIO->PCNF1 = 0x02040000 | radio_state.max_payload;
 
-    // Enable automatic 16bit CRC generation and checking, and configure how the CRC is calculated.
-    NRF_RADIO->CRCCNF = RADIO_CRCCNF_LEN_Two;
-    NRF_RADIO->CRCINIT = 0xFFFF;
-    NRF_RADIO->CRCPOLY = 0x11021;
+    // // Enable automatic 16bit CRC generation and checking, and configure how the CRC is calculated.
+    // NRF_RADIO->CRCCNF = RADIO_CRCCNF_LEN_Two;
+    // NRF_RADIO->CRCINIT = 0xFFFF;
+    // NRF_RADIO->CRCPOLY = 0x11021;
 
-    // Set the start random value of the data whitening algorithm. This can be any non zero number.
-    NRF_RADIO->DATAWHITEIV = 0x18;
+    // // Set the start random value of the data whitening algorithm. This can be any non zero number.
+    // NRF_RADIO->DATAWHITEIV = 0x18;
 
-    // set receive buffer
-    NRF_RADIO->PACKETPTR = (uint32_t)rx_buf;
+    // // set receive buffer
+    // NRF_RADIO->PACKETPTR = (uint32_t)rx_buf;
 
-    // configure interrupts
-    NRF_RADIO->INTENSET = 0x00000008;
-    NVIC_ClearPendingIRQ(RADIO_IRQn);
-    NVIC_EnableIRQ(RADIO_IRQn);
+    // // configure interrupts
+    // NRF_RADIO->INTENSET = 0x00000008;
+    // NVIC_ClearPendingIRQ(RADIO_IRQn);
+    // NVIC_EnableIRQ(RADIO_IRQn);
 
-    NRF_RADIO->SHORTS |= RADIO_SHORTS_ADDRESS_RSSISTART_Msk;
+    // NRF_RADIO->SHORTS |= RADIO_SHORTS_ADDRESS_RSSISTART_Msk;
 
-    // enable receiver
-    NRF_RADIO->EVENTS_READY = 0;
-    NRF_RADIO->TASKS_RXEN = 1;
-    while (NRF_RADIO->EVENTS_READY == 0);
+    // // enable receiver
+    // NRF_RADIO->EVENTS_READY = 0;
+    // NRF_RADIO->TASKS_RXEN = 1;
+    // while (NRF_RADIO->EVENTS_READY == 0);
 
-    NRF_RADIO->EVENTS_END = 0;
-    NRF_RADIO->TASKS_START = 1;
-    */
+    // NRF_RADIO->EVENTS_END = 0;
+    // NRF_RADIO->TASKS_START = 1;
 }
 
 void radio_send(const void *buf, size_t len, const void *buf2, size_t len2) {
-  /*
     ensure_enabled();
-
+  /*
     // construct the packet
     // note: we must send from RAM
     size_t max_len = NRF_RADIO->PCNF1 & 0xff;
@@ -241,11 +239,16 @@ void radio_send(const void *buf, size_t len, const void *buf2, size_t len2) {
     NVIC_ClearPendingIRQ(RADIO_IRQn);
     NVIC_EnableIRQ(RADIO_IRQn);
   */
+    uint8_t send_buf[2048];
+    uint32_t send_len = len + len2;
+    memcpy(send_buf, buf, len);
+    memcpy(send_buf + len, buf2, len2);
+    simulator_radio_send(send_buf, send_len);
 }
 
 static mp_obj_t radio_receive(bool typed_packet) {
-  /*
     ensure_enabled();
+  /*
 
     // disable the radio irq while we receive the packet
     NVIC_DisableIRQ(RADIO_IRQn);
@@ -281,13 +284,28 @@ static mp_obj_t radio_receive(bool typed_packet) {
 
     return ret;
   */
+
+    uint8_t buf[2048];
+    uint32_t len = sizeof(buf);
+    if (simulator_radio_receive(buf, &len)) {
+        mp_obj_t ret;
+        if (!typed_packet) {
+            ret = mp_obj_new_bytes(buf, len); // if it raises the radio irq remains disabled...
+	} else if (len >= 2 && buf[0] == 1 && buf[1] == 0 && buf[2] == 1) {
+	    ret = mp_obj_new_str((char*)buf + 3, len - 3, false); // if it raises the radio irq remains disabled...
+	} else {
+	    nlr_raise(mp_obj_new_exception_msg(&mp_type_ValueError, "received packet is not a string"));
+	}
+	return ret;
+    } else {
+        return mp_const_none;
+    }
 }
 
 /*****************************************************************************/
 // MicroPython bindings and module
 
 STATIC mp_obj_t mod_radio_reset(void) {
-  /*
     radio_state.max_payload = RADIO_DEFAULT_MAX_PAYLOAD;
     radio_state.queue_len = RADIO_DEFAULT_QUEUE_LEN;
     radio_state.channel = RADIO_DEFAULT_CHANNEL;
@@ -295,7 +313,6 @@ STATIC mp_obj_t mod_radio_reset(void) {
     radio_state.base0 = RADIO_DEFAULT_BASE0;
     radio_state.prefix0 = RADIO_DEFAULT_PREFIX0;
     radio_state.data_rate = RADIO_DEFAULT_DATA_RATE;
-  */
     return mp_const_none;
 }
 MP_DEFINE_CONST_FUN_OBJ_0(mod_radio_reset_obj, mod_radio_reset);
@@ -306,7 +323,7 @@ STATIC mp_obj_t mod_radio_config(size_t n_args, const mp_obj_t *pos_args, mp_map
     if (n_args != 0) {
         nlr_raise(mp_obj_new_exception_msg(&mp_type_TypeError, "arguments must be keyword arguments"));
     }
-    /*
+
     // make a copy of the radio state so we don't change anything if there are value errors
     radio_state_t new_state = radio_state;
 
@@ -389,38 +406,36 @@ STATIC mp_obj_t mod_radio_config(size_t n_args, const mp_obj_t *pos_args, mp_map
             // only registers changed so make the changes go through efficiently
 
             // disable radio
-            NVIC_DisableIRQ(RADIO_IRQn);
-            NRF_RADIO->EVENTS_DISABLED = 0;
-            NRF_RADIO->TASKS_DISABLE = 1;
-            while (NRF_RADIO->EVENTS_DISABLED == 0);
+            // NVIC_DisableIRQ(RADIO_IRQn);
+            // NRF_RADIO->EVENTS_DISABLED = 0;
+            // NRF_RADIO->TASKS_DISABLE = 1;
+            // while (NRF_RADIO->EVENTS_DISABLED == 0);
 
             // change state
             radio_state = new_state;
-            NRF_RADIO->TXPOWER = radio_state.power_dbm;
-            NRF_RADIO->FREQUENCY = radio_state.channel;
-            NRF_RADIO->MODE = radio_state.data_rate;
-            NRF_RADIO->BASE0 = radio_state.base0;
-            NRF_RADIO->PREFIX0 = radio_state.prefix0;
+	    simulator_radio_config(radio_state.channel, radio_state.base0, radio_state.prefix0, radio_state.data_rate);
+            // NRF_RADIO->TXPOWER = radio_state.power_dbm;
+            // NRF_RADIO->FREQUENCY = radio_state.channel;
+            // NRF_RADIO->MODE = radio_state.data_rate;
+            // NRF_RADIO->BASE0 = radio_state.base0;
+            // NRF_RADIO->PREFIX0 = radio_state.prefix0;
 
-            // need to set RXEN for FREQUENCY decision point
-            NRF_RADIO->EVENTS_READY = 0;
-            NRF_RADIO->TASKS_RXEN = 1;
-            while (NRF_RADIO->EVENTS_READY == 0);
+            // // need to set RXEN for FREQUENCY decision point
+            // NRF_RADIO->EVENTS_READY = 0;
+            // NRF_RADIO->TASKS_RXEN = 1;
+            // while (NRF_RADIO->EVENTS_READY == 0);
 
-            // need to set START for BASE0 and PREFIX0 decision point
-            NRF_RADIO->EVENTS_END = 0;
-            NRF_RADIO->TASKS_START = 1;
+            // // need to set START for BASE0 and PREFIX0 decision point
+            // NRF_RADIO->EVENTS_END = 0;
+            // NRF_RADIO->TASKS_START = 1;
 
-            NVIC_ClearPendingIRQ(RADIO_IRQn);
-            NVIC_EnableIRQ(RADIO_IRQn);
+            // NVIC_ClearPendingIRQ(RADIO_IRQn);
+            // NVIC_EnableIRQ(RADIO_IRQn);
         }
     }
-    */
     return mp_const_none;
-    /*
 value_error:
     nlr_raise(mp_obj_new_exception_msg_varg(&mp_type_ValueError, "value out of range for argument '%q'", arg_name));
-    */
 }
 MP_DEFINE_CONST_FUN_OBJ_KW(mod_radio_config_obj, 0, mod_radio_config);
 
@@ -474,10 +489,10 @@ STATIC const mp_map_elem_t radio_module_globals_table[] = {
     { MP_OBJ_NEW_QSTR(MP_QSTR_receive_bytes), (mp_obj_t)&mod_radio_receive_bytes_obj },
     { MP_OBJ_NEW_QSTR(MP_QSTR_send), (mp_obj_t)&mod_radio_send_obj },
     { MP_OBJ_NEW_QSTR(MP_QSTR_receive), (mp_obj_t)&mod_radio_receive_obj },
-    /*
+
     { MP_OBJ_NEW_QSTR(MP_QSTR_RATE_250KBIT), MP_OBJ_NEW_SMALL_INT(RADIO_MODE_MODE_Nrf_250Kbit) },
     { MP_OBJ_NEW_QSTR(MP_QSTR_RATE_1MBIT), MP_OBJ_NEW_SMALL_INT(RADIO_MODE_MODE_Nrf_1Mbit) },
-    { MP_OBJ_NEW_QSTR(MP_QSTR_RATE_2MBIT), MP_OBJ_NEW_SMALL_INT(RADIO_MODE_MODE_Nrf_2Mbit) },*/
+    { MP_OBJ_NEW_QSTR(MP_QSTR_RATE_2MBIT), MP_OBJ_NEW_SMALL_INT(RADIO_MODE_MODE_Nrf_2Mbit) },
 };
 
 STATIC MP_DEFINE_CONST_DICT(radio_module_globals, radio_module_globals_table);
